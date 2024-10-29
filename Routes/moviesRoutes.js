42e172b2b0d0af07f8b12158d575b3bdac3c9534
AbdulArif <abdularif.dgp@gmail.com>
2024-10-29 15:00:43 +0530

    update

--- Routes/moviesRoutes.js
+++ Routes/moviesRoutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const moviesController = require('./../Controllers/moviesController')
 
+router.param('id', (req, res, next, value) => {
+    console.log('Movies id is ' + value)
+    next();
+})
+
 router.route('/')
     .get(moviesController.getAllMovies)
     .post(moviesController.createMovie)
