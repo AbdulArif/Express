9505ef5da5d60e0408ce4105fc9f5b463ca50853
AbdulArif <abdularif.dgp@gmail.com>
2024-10-29 17:14:19 +0530

    upadte

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 
 // Create ser server
 const app =require('./app')
+console.log(app.get('env'));
+// console.log(process.env);
+
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
