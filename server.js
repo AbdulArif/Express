0c9742ba43336058484619fa0a12639cf5c62991
AbdulArif <abdularif.dgp@gmail.com>
2024-10-31 12:29:01 +0530

    console.log(process.env.PORT);

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 
 // Create ser server
+const dotenv = require('dotenv')
+dotenv.config({path: './Config.env'})
 const app =require('./app')
 console.log(app.get('env'));
-// console.log(process.env);
+
+// console.log(process.env.PORT);
 
 
 const port = process.env.PORT || 3000;
