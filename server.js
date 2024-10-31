6bc3697eed1e10d3e7ab58c2ec3d27d8a82d1a52
AbdulArif <abdularif.dgp@gmail.com>
2024-10-31 12:16:24 +0530

    update port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ console.log(app.get('env'));
 // console.log(process.env);
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 
