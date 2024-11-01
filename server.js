343dfadfd48402d4cf230c7165e92d0018244586
AbdulArif <abdularif.dgp@gmail.com>
2024-11-01 20:04:44 +0530

    update

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 
 // Create ser server
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-dotenv.config({path: './Config.env'})
-const app =require('./app')
+dotenv.config({ path: './Config.env' })
+const app = require('./app')
 console.log(app.get('env'));
 
 // console.log(process.env.PORT);
-
+// mongoose.connect(process.env.CONN_STR, {
+//     userNewUrlParser: true
+// }).then((conn) => {
+//     console.log(conn)
+//     console.log("DB Connection Successfull")
+// }).catch((error)=>{
+// console.log("Some error has occured in Dadatbase connection")
+// })
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
